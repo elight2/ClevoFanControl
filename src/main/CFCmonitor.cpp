b5dbc347171a6d9dce8d3a8050570d31020530ea
elight <elight2@163.com>
2024-11-19 14:47:27 +0800

    latest

---
+++ src/main/CFCmonitor.cpp
@@ -0,0 +1,24 @@
+#include "CFCmonitor.h"
+#include <qobject.h>
+
+CFCmonitor::CFCmonitor(QWidget *parent) : QWidget(parent) {
+    this->setAttribute(Qt::WA_QuitOnClose, 0);
+    this->setFixedSize(200,300);
+    ui.setupUi(this);
+}
+
+void CFCmonitor::updateValue(int index, int speed, int rpm, int temperature) {
+    //qDebug()<<"CFCmonitor::updateValue ";
+    if(index==1) {
+        ui.label_2->setText(QString::number(temperature));
+        ui.label_6->setText(QString::number(speed));
+        ui.label_11->setText(QString::number(rpm));
+    }
+    else if(index==2) {
+        ui.label_4->setText(QString::number(temperature));
+        ui.label_8->setText(QString::number(speed));
+        ui.label_12->setText(QString::number(rpm));
+    }
+    //qDebug()<<"CFCmonitor::updateValue finish";
+    return;
+}
---
+++ src/main/ClevoFanControl.h
@@ -0,0 +1,65 @@
+#include <QtWidgets/qaction.h>
+#include <QtCore/qlist.h>
+#include <QtGui/qicon.h>
+#include <QtWidgets/qsystemtrayicon.h>
+#include <QtWidgets/qmenu.h>
+#include <QtWidgets/qpushbutton.h>
+#include <QtWidgets/qtablewidget.h>
+#include <QtWidgets/qmessagebox.h>
+#include <QtWidgets/qactiongroup.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qfile.h>
+#include <QtCore/qdebug.h>
+#include <QtWidgets/qapplication.h>
+#include <qaction.h>
+#include <qkeysequence.h>
+
+#include "ConfigManager.h"
+#include "CFCmonitor.h"
+#include "CFCconfig.h"
+#include "FanController.h"
+
+using json = nlohmann::json;
+
+class ClevoFanControl : public QWidget {
+Q_OBJECT
+
+public:
+    ClevoFanControl(QWidget *parent = nullptr);
+    ~ClevoFanControl();
+
+private:
+    QDir CFCpath = QDir::current();
+    ConfigManager *cfgMgr=nullptr;
+    //ui
+    QSystemTrayIcon *TrayIcon = NULL;
+    QMenu * trayMainMenu=nullptr;
+    QMenu * trayProfilesMenu=nullptr;
+    QMenu * trayCommandsMenu=nullptr;
+    QAction *trayMonitorAction=nullptr;
+    QAction *trayConfigAction=nullptr;
+    QAction *trayMaxSpeedAction=nullptr;
+    QAction *traySpeedLimitAction=nullptr;
+    QAction *trayStaticSpeedAction=nullptr;
+    QAction *trayClevoAutoAction=nullptr;
+    QAction *trayMonitorGpuAction=nullptr;
+    QAction *trayExitAction=nullptr;
+    QList<QAction*> profileActions;
+    QList<QAction*>commandAcions;
+    QActionGroup *profilesGroup;
+    //window
+    CFCmonitor *monitor=nullptr;
+    CFCconfig *configWindow=nullptr;
+    //fan
+    CpuFanController *cpuFan=nullptr;
+    GpuFanController *gpuFan=nullptr;
+
+    void buildUi();
+    void initTrayEntry(QAction *&action,QString text, bool checkable);
+    void executeCommand();
+    //ui
+    void updateMonitorSlot(int index, int speed, int rpm, int temperature);
+    void cfgTrayToRam();
+    void cfgRamToTray();
+    void trayUpdated();
+};
